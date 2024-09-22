058181c7a4cfccfe48484f7e6502b75e43e7c5b5
Your Name <your_email@example.com>
2024-09-22 19:44:21 +0530

    footer adjustment

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -7,6 +7,7 @@ function Footer() {
   <nav>
     <h6 className="footer-title">Services</h6>
     <a className="link link-hover">Branding</a>
+    <a className="link link-hover">Branding</a>
     <a className="link link-hover">Design</a>
     <a className="link link-hover">Marketing</a>
     <a className="link link-hover">Advertisement</a>
