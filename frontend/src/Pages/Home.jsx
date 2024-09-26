62e4af980cd28397a316abe1744178e041c376c9
Your Name <your_email@example.com>
2024-09-26 19:55:45 +0530

    added skeleton

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -31,7 +31,16 @@ useEffect(() => {
   }
 }, [products]);
 
-if (loading) return <h1>Loading....</h1>;
+if (loading) return <h1><div className="flex min-h-screen w-52 flex-col gap-4">
+<div className="flex items-center gap-4">
+  <div className="skeleton h-16 w-16 shrink-0 rounded-full"></div>
+  <div className="flex flex-col gap-4">
+    <div className="skeleton h-4 w-20"></div>
+    <div className="skeleton h-4 w-28"></div>
+  </div>
+</div>
+<div className="skeleton h-32 w-full"></div>
+</div></h1>;
 if (error) return <h1>Error:{error.message}</h1>;
 
 
