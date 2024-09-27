d4688bdc1f24fbeef4f71a445d24b6f81e056665
Your Name <your_email@example.com>
2024-09-27 08:33:40 +0530

    cors update

--- backend/app.js
+++ backend/app.js
@@ -17,8 +17,7 @@ const orderRoute=require("./Routes/orderRoute")
 // CORS options configuration
 const allowedOrigins = [
   'http://localhost:5173', // Local development
-  'https://e-commerce-puce-three-50.vercel.app', // Deployed frontend
-  'https://e-commerce-server-ecru.vercel.app/',
+
    "https://e-commerce-frontend-blush.vercel.app",
    "https://e-commerce-server-ecru.vercel.app/"
 ];
